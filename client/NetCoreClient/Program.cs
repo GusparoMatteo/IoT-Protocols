9be468b43212838b939b7628d8f3526d3209c24f
IIOTDenis <133649852+IIOTDenis@users.noreply.github.com>
2024-04-04 17:47:02 +0200

    Update Client Solution
    Add Level Battery

---
+++ client/NetCoreClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using NetCoreClient.Sensors;
+using NetCoreClient.Protocols;
+
+// define sensors
+List<ISensorInterface> sensors = new();
+sensors.Add(new VirtualSpeedSensor());
+
+// define protocol
+ProtocolInterface protocol = new Http("https://9196-217-201-225-94.ngrok-free.app");
+
+// send data to server
+while (true)
+{
+    foreach (ISensorInterface sensor in sensors)
+    {
+        var sensorValue = sensor.ToJson();
+
+        protocol.Send(sensorValue);
+
+        Console.WriteLine("Data sent: " + sensorValue);
+
+        Thread.Sleep(1000);
+    }
+
+}
