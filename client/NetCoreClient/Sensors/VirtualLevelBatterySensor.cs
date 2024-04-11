8f79ba76c73fd63e3eb5157aa47192e9596bf561
IIOTDenis <133649852+IIOTDenis@users.noreply.github.com>
2024-04-11 12:40:51 +0200

    Client - Protocollo Http Funzionante
    Invia un json contenente il livello della batteria al server,
    Readme aggiornato a dovere

---
+++ client/NetCoreClient/Sensors/VirtualLevelBatterySensor.cs
@@ -0,0 +1,25 @@
+﻿using NetCoreClient.ValueObjects;
+using System.Text.Json;
+
+namespace NetCoreClient.Sensors
+{
+    class VirtualLevelBatterySensor : ILevelBatterySensorInterface, ISensorInterface
+    {
+        private readonly Random Random;
+
+        public VirtualLevelBatterySensor()
+        {
+            Random = new Random();
+        }
+
+        public int LevelBattery()
+        {
+            return new LevelBattery(Random.Next(100)).Value;
+        }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(LevelBattery());
+        }
+    }
+}
--- client/NetCoreClient/obj/Debug/net6.0/NetCoreClient.AssemblyInfo.cs
+++ client/NetCoreClient/obj/Debug/net6.0/NetCoreClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetCoreClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33325275c5943fb502d4708bda8427b5f76e138c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetCoreClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetCoreClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
